4ee0a42c021c071353c953a38eb0972cb7c76559
Donghoon Jeon <dhjeon1993@gmail.com>
2019-09-27 14:39:56 +0900

    Spring Test 추가

--- src/springbook/user/test/UserDaoTest.java
+++ src/springbook/user/test/UserDaoTest.java
@@ -4,10 +4,14 @@ import javafx.application.Application;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import springbook.user.dao.UserDao;
 import springbook.user.domain.User;
 
@@ -16,8 +20,11 @@ import java.sql.SQLException;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
-
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "/resources/config/applicationContext.xml")
 public class UserDaoTest {
+    @Autowired
+    private ApplicationContext context;
 
     private UserDao dao;
 
@@ -27,11 +34,6 @@ public class UserDaoTest {
 
     @Before
     public void setUp() {
-        ApplicationContext context =
-                new ClassPathXmlApplicationContext(
-                        "resources/config/applicationContext.xml"
-                );
-
         this.dao = context.getBean("userDao", UserDao.class);
 
         user1 = new User("gyumee", "박성철", "springno1");
