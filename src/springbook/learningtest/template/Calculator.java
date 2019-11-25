a1a4f18096d66c985d7eb8732de9b4df91c71fee
Dong-Hoon Jeon <dhjeon1993@gmail.com>
2019-11-25 16:35:09 +0900

    concatenate 메소드 추가

--- src/springbook/learningtest/template/Calculator.java
+++ src/springbook/learningtest/template/Calculator.java
@@ -73,4 +73,15 @@ public class Calculator {
                 };
         return lineReadTemplate(filepath, multiplyCallback, 1);
     }
+
+    public String concatenate(String filepath) throws IOException {
+        LineCallback<String> concatenateCallback =
+                new LineCallback<String>() {
+                    @Override
+                    public String doSomethingWithLine(String line, String value) {
+                        return value + line;
+                    }
+                };
+        return lineReadTemplate(filepath, concatenateCallback, "");
+    }
 }
