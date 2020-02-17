ab5fad154e376c6e12c1d2d29df59fc11c72f79b
kyungjunyim <killop2000@gmail.com>
2020-02-17 16:28:02 +0900

    Hello Proxy

---
+++ src/springbook/learningtest/proxy/HelloTest.java
@@ -0,0 +1,27 @@
+package springbook.learningtest.proxy;
+
+import org.junit.Test;
+
+import java.lang.reflect.Proxy;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class HelloTest {
+    @Test
+    public void simpleProxy() {
+        Hello hello = new HelloTarget();
+        assertThat(hello.sayHello("Toby"), is("Hello Toby"));
+        assertThat(hello.sayHi("Toby"), is("Hi Toby"));
+        assertThat(hello.sayThankYou("Toby"), is("Thank You Toby"));
+
+        Hello proxiedHello = (Hello) Proxy.newProxyInstance(
+                getClass().getClassLoader(),
+                new Class[] { Hello.class },
+                new UppercaseHandler(new HelloTarget())
+        );
+        assertThat(proxiedHello.sayHello("Toby"), is("HELLO TOBY"));
+        assertThat(proxiedHello.sayHi("Toby"), is("HI TOBY"));
+        assertThat(proxiedHello.sayThankYou("Toby"), is("THANK YOU TOBY"));
+    }
+}
