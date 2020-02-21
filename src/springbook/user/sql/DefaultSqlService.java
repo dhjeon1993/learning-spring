01d0451f1367e24b0f35707d6dbd6b7c5f5af7fd
DONGHOON JEON <dhjeon1993@gmail.com>
2020-02-21 09:16:19 +0900

    분리시킨 SqlService의 각 기본 구현체 작성

---
+++ src/springbook/user/sql/DefaultSqlService.java
@@ -0,0 +1,8 @@
+package springbook.user.sql;
+
+public class DefaultSqlService extends BaseSqlService {
+    public DefaultSqlService() {
+        setSqlReader(new JaxbXmlSqlReader());
+        setSqlRegistry(new HashMapSqlRegistry());
+    }
+}
