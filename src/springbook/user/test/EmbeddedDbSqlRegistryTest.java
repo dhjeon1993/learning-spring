a74263de92dc88571625949072947712318f50e6
DONGHOON JEON <dhjeon1993@gmail.com>
2020-03-26 17:33:29 +0900

    트랜잭션 테스트

--- src/springbook/user/test/EmbeddedDbSqlRegistryTest.java
+++ src/springbook/user/test/EmbeddedDbSqlRegistryTest.java
@@ -1,11 +1,18 @@
 package springbook.user.test;
 
 import org.junit.After;
+import org.junit.Test;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import springbook.issuetracker.sqlservice.UpdatableSqlRegistry;
 import springbook.user.sql.EmbeddedDbSqlRegistry;
+import springbook.user.sql.SqlUpdateFailureException;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.fail;
 
 public class EmbeddedDbSqlRegistryTest extends AbstractUpdatableSqlRegistryTest {
     EmbeddedDatabase db;
@@ -22,6 +29,22 @@ public class EmbeddedDbSqlRegistryTest extends AbstractUpdatableSqlRegistryTest 
         embeddedDbSqlRegistry.setDataSource(db);
 
         return embeddedDbSqlRegistry;
+    }
+
+    @Test
+    public void transactionalUpdate() {
+        checkFindResult("SQL1", "SQL2", "SQL3");
+
+        Map<String, String> sqlmap = new HashMap<>();
+        sqlmap.put("KEY1", "Modified1");
+        sqlmap.put("KEY9999!@#$", "Modified9999");
+
+        try {
+            sqlRegistry.updateSql(sqlmap);
+            fail();
+        } catch(SqlUpdateFailureException e) {}
+
+        checkFindResult("SQL1", "SQL2", "SQL3");
     }
 
     @After
